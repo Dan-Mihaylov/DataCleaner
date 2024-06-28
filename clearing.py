7597595e8b5018c1af3a2f2b78f34ec78a76cb38
Dan-Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2024-06-28 17:05:55 +0100

    Worked on core functionality, added themes, added samples and screenshots

--- clearing.py
+++ clearing.py
@@ -3,20 +3,42 @@ import pandas as pd
 
 class DataCleaner:
     """
-    Takes only path: str
+    If split, then splits by 200 and outputs several files, otherwise outputs one file
+
+    path: str
+    split: bool
     """
 
-    def __init__(self, path: str):
+    def __init__(self, path: str, split: bool):
         self.path = path
+        self.split = split
+        self.ignore_zonal = '@zonal.co.uk'
+        self.ignore_test = 'test'
 
     def clean_data(self):
+        """
+        Reads the .csv file from the path given, and cleans the data.
+            1 - Converts Email to lowercase
+            2 - Removes duplicate Emails and notna values
+            3 - Ignores emails that are in the ignore_emails list
+
+        :return: bool or raises
+        """
         try:
             ignore_emails = self.get_ignore_emails()
             data = pd.read_csv(self.path)
+
+            # Remove Empty Values
             data['Email'] = data['Email'].str.lower()
             unique_emails = data.drop_duplicates(['Email'])
             drop_na = unique_emails[unique_emails['Email'].notna()]
             result = drop_na[['Email', 'First Name', 'Last Name']]
+            result.loc[:, 'Email'] = result['Email'].astype(str)
+
+            # Remove zonal support emails and tests
+            result = result[(~result.Email.str.endswith(self.ignore_zonal) & ~result.Email.str.contains(self.ignore_test))]
+
+            # Remove emails that are in the ignore list
             result = result[~result['Email'].isin(ignore_emails)]
             self.save(result)
             return True
@@ -26,19 +48,54 @@ class DataCleaner:
 
     @staticmethod
     def get_ignore_emails():
+        """
+        Checks whether you have selected emails to ignore, and returns them formated without the /n at the end.
+
+        :return: list
+        """
         try:
             with open('_internal/ignore_emails.txt', 'r') as f:
                 lines = f.readlines()
                 if len(lines) > 0:
-                    return [line[:-1] for line in lines]
+                    return [line.replace('\n', '') for line in lines] # takes off the new line
                 return []
-        except FileNotFoundError:
+        except Exception:
             return []
 
     def save(self, result: pd.DataFrame):
-        new_file_name = 'Cleaned Bookings.csv'
-        new_file_path = self.path.split('/')
-        new_file_path[-1] = new_file_name
-        new_file_path = '/'.join(new_file_path)
-        result.to_csv(new_file_path, index=False)
+        """
+        Creates an output file in the same dir as the .csv input file or several files depending on self.split value
+
+        :param result: pd.DataFrame
+        :return: None
+        """
+        output_list = self.split_two_hundred(result) if self.split else [result]
+        name = ' 200' if len(output_list) > 1 else ''
+
+        for index in range(len(output_list)):
+            new_file_name = f'Cleaned Bookings{name}-{index + 1}.csv'
+            new_file_path = self.path.split('/')
+            new_file_path[-1] = new_file_name
+            new_file_path = '/'.join(new_file_path)
+            output_list[index].to_csv(new_file_path, index=False)
         return
+
+    @staticmethod
+    def split_two_hundred(entry_df: pd.DataFrame):
+        """
+        Takes the DataFrame and splits the results by 200 entries, if self.split
+
+        :param entry_df: pd.DataFrame
+        :return: list
+        """
+        max_rows = 200
+        num_splits = (len(entry_df) // max_rows) + 1
+
+        output_list = []
+
+        for i in range(num_splits):
+            start_index = i * max_rows
+            end_index = min(start_index + max_rows, len(entry_df))
+            output_list.append(entry_df.iloc[start_index:end_index])
+
+        return output_list
--- main.py
+++ main.py
@@ -1,84 +1,188 @@
 from tkinter import *
-from tkinter import filedialog, messagebox, LabelFrame, Text
+from tkinter import filedialog, messagebox, LabelFrame, Text, Checkbutton, BooleanVar
 from clearing import DataCleaner
 
 
 class MainWindow:
     def __init__(self, root):
         self.root = root
-        self.root.geometry("800x600")
+        self.root.geometry("480x720")
         self.root.title("Email Management")
         self.root.iconbitmap('_internal/logo.ico')
-
-        # Create the LabelFrame
-        self.labelframe = LabelFrame(self.root, borderwidth=5, relief="flat", padx=5, pady=5)
-        self.labelframe.pack(fill="both", expand=True, padx=10, pady=10)
-        self.labelframe.configure(background="#444")
 
         # Add widgets to the LabelFrame
         self.filepath = None
         self.create_widgets()
 
-        self.root.configure(background="#444")
+    def show_menu(self, event):
+        # Position the menu right below the menubutton
+        x = self.theme_selectbox.winfo_rootx()
+        y = self.theme_selectbox.winfo_rooty() + self.theme_selectbox.winfo_height()
+        self.custom_menu.post(x, y)
+
+    def destroy_widgets(self):
+        for widget in self.root.winfo_children():
+            widget.destroy()
 
     def create_widgets(self):
+
+        self.destroy_widgets()
+
+        # Create the LabelFrame
+        self.labelframe = LabelFrame(self.root, borderwidth=5, relief="flat", padx=5, pady=5)
+        self.labelframe.pack(fill="both", expand=True, padx=10, pady=10)
+
+        # Theme select box
+        self.theme_var = StringVar(value="Dark")
+
+        # Create a custom Menu for the OptionMenu
+        self.custom_menu = Menu(self.root, tearoff=0)
+
+        # Add options to the custom Menu
+        self.custom_menu.add_radiobutton(label="Dark", variable=self.theme_var, value="Dark", command=self.configure_appearance)
+        self.custom_menu.add_radiobutton(label="Light", variable=self.theme_var, value="Light", command=self.configure_appearance)
+
+        # Create the Menubutton
+        self.theme_selectbox = Menubutton(
+            self.root,
+            textvariable=self.theme_var,
+            indicatoron=False
+        )
+        self.theme_selectbox["menu"] = self.custom_menu
+        self.theme_selectbox.place(anchor="nw")
+        self.theme_selectbox.bind('<Button-1>', self.show_menu)
+
         # Logo?
         self.logo = PhotoImage(file="_internal/logo.png")
         self.logo = self.logo.subsample(20, 20)
         self.logo_label = Label(self.labelframe, image=self.logo)
-        self.logo_label.configure(background="#444")
         self.logo_label.pack()
         # Instructions
         self.step1 = Label(self.labelframe, text="Step 1\nChose the .csv file to clean.")
-        self.step1.configure(background="#444", foreground="#bbb", font="Helvetica 16")
-        self.step1.pack(pady=10)
+        self.step1.pack(pady=(50, 10))
 
         self.step2 = Label(self.labelframe, text="Step 2\nIf you need, add emails to exclude from the list.")
-        self.step2.configure(background="#444", foreground="#bbb", font="Helvetica 16")
         self.step2.pack(pady=10)
 
         self.step3 = Label(self.labelframe, text="Step 3\nClean emails.")
-        self.step3.configure(background="#444", foreground="#bbb", font="Helvetica 16")
         self.step3.pack(pady=10)
+
+        # Boolean & checkbutton to see if you want to split by 200;
+        self.split_by_200 = BooleanVar()
+        self.split_checkbox = Checkbutton(
+            self.labelframe,
+            text="Split by 200",
+            variable=self.split_by_200,
+        )
+        self.split_checkbox.pack(pady=10)
 
         # Button to choose file
         self.choose_file_button = Button(self.labelframe, text="Choose File", command=self.choose_file)
-        self.choose_file_button.configure(
-            background="#333",
-            foreground="#bbb",
-            font="Helvetica 16",
-            width="100",
-            activebackground="#555",
-            activeforeground="#bbb",
-            borderwidth=0,
-        )
-        self.choose_file_button.pack(pady=10)
+        self.choose_file_button.pack(pady=(50, 10))
 
         # Button to exclude emails
         self.exclude_emails_button = Button(self.labelframe, text="Exclude Emails", command=self.open_exclude_window)
-        self.exclude_emails_button.configure(
-            background="#333",
-            foreground="#bbb",
-            font="Helvetica 16",
-            width="100",
-            activebackground="#555",
-            activeforeground="#bbb",
-            borderwidth=0,
-        )
         self.exclude_emails_button.pack(pady=10)
 
         # Button to clean emails
         self.clean_emails_button = Button(self.labelframe, text="Clean Emails", command=self.clean_emails)
-        self.clean_emails_button.configure(
-            background="#333",
-            foreground="#bbb",
-            font="Helvetica 16",
-            width="100",
-            activebackground="#555",
-            activeforeground="#bbb",
-            borderwidth=0,
-        )
         self.clean_emails_button.pack(pady=10)
+
+        self.configure_appearance()
+
+    def configure_appearance(self):
+
+        background_dark = '#444'
+        background_button_dark = '#333'
+        active_background_dark = '#555'
+        active_foreground_dark = '#bbb'
+        foreground_dark = '#bbb'
+        background_email_dark = '#666'
+        foreground_email_dark = '#ccc'
+        
+        background_light = '#fff'
+        background_button_light = '#eee'
+        active_background_light = '#ddd'
+        active_foreground_light = '#444'
+        foreground_light = '#555'
+        background_email_light = '#eee'
+        foreground_email_light = '#555'
+
+        self.font = 'Helvetica 14'
+        self.email_font = 'Helvetica 12'
+        self.background = background_dark if self.theme_var.get() == 'Dark' else background_light
+        self.background_button = background_button_dark if self.theme_var.get() == 'Dark' else background_button_light
+        self.active_background = active_background_dark if self.theme_var.get() == 'Dark' else active_background_light
+        self.active_foreground = active_foreground_dark if self.theme_var.get() == 'Dark' else active_foreground_light
+        self.foreground = foreground_dark if self.theme_var.get() == 'Dark' else foreground_light
+        self.background_email = background_email_dark if self.theme_var.get() == 'Dark' else background_email_light
+        self.foreground_email = foreground_email_dark if self.theme_var.get() == 'Dark' else foreground_email_light
+
+        self.labelframe.configure(background=self.background)
+        self.root.configure(background=self.background)
+
+        self.custom_menu.configure(bg=self.background, fg=self.foreground, activebackground=self.active_background, activeforeground=self.foreground)
+
+        self.theme_selectbox.configure(
+            borderwidth=0,
+            relief="flat",
+            bg=self.background,
+            fg=self.foreground,
+            font="Helvetica 8",
+            activebackground=self.active_background,
+            activeforeground=self.foreground
+        )
+
+        self.logo_label.configure(background=self.background)
+
+        self.step1.configure(background=self.background, foreground=self.foreground, font=self.font)
+
+        self.step2.configure(background=self.background, foreground=self.foreground, font=self.font)
+
+        self.step3.configure(background=self.background, foreground=self.foreground, font=self.font)
+
+        self.split_checkbox.configure(
+            bg=self.background,
+            fg=self.foreground,
+            font=self.font,
+            selectcolor=self.background,
+            activebackground=self.background,
+            activeforeground=self.active_foreground,
+            cursor='hand2',
+        )
+
+        self.choose_file_button.configure(
+            background=self.background_button,
+            foreground=self.foreground,
+            font=self.font,
+            width="100",
+            activebackground=self.active_background,
+            activeforeground=self.foreground,
+            borderwidth=0,
+            cursor='hand2',
+        )
+
+        self.exclude_emails_button.configure(
+            background=self.background_button,
+            foreground=self.foreground,
+            font=self.font,
+            width="100",
+            activebackground=self.active_background,
+            activeforeground=self.foreground,
+            borderwidth=0,
+            cursor='hand2',
+        )
+
+        self.clean_emails_button.configure(
+            background=self.background_button,
+            foreground=self.foreground,
+            font=self.font,
+            width="100",
+            activebackground=self.active_background,
+            activeforeground=self.foreground,
+            borderwidth=0,
+            cursor='hand2',
+        )
 
     def choose_file(self):
         self.filepath = filedialog.askopenfilename()
@@ -89,43 +193,43 @@ class MainWindow:
         self.exclude_window.title("Exclude Emails")
         self.exclude_window.iconbitmap('_internal/logo.ico')
         self.exclude_window.geometry("800x500")
-        self.exclude_window.configure(background="#444")
+        self.exclude_window.configure(background=self.background)
 
         text = ("Enter emails to be excluded from the list on new line ex:\n"
                 "email1@mail.com\n"
                 "email2@mail.com\n")
         self.label = Label(self.exclude_window, text=text, wraplength=600)
-        self.label.configure(background="#444", foreground="#aaa", font="Helvetica 15")
+        self.label.configure(background=self.background, foreground=self.foreground, font=self.email_font)
         self.label.pack(pady=10)
 
         # Create a text widget for entering emails
         self.email_text = Text(self.exclude_window, width=60, height=15)
-        self.email_text.configure(background="#555", foreground="#ccc", font="Helvetica 12", borderwidth=0)
+        self.email_text.configure(background=self.background_email, foreground=self.foreground_email, font=self.email_font, borderwidth=0)
         self.email_text.pack(pady=10)
 
         # Button to save emails to ignore_emails.txt
         self.save_button = Button(self.exclude_window, text="Exclude Emails", command=self.save_emails)
         self.save_button.configure(
-            background="#333",
-            foreground="#bbb",
-            font="Helvetica 16",
+            background=self.background_button,
+            foreground=self.foreground,
+            font=self.font,
             width="60",
-            activebackground="#555",
-            activeforeground="#bbb",
+            activebackground=self.active_background,
+            activeforeground=self.foreground,
             borderwidth=0,
         )
         self.save_button.pack(pady=10)
 
     def save_emails(self):
         emails = self.email_text.get("1.0", END)
-        with open("_internal/ignore_emails.txt", "w") as file:
+        with open("_internal/ignore_emails.txt", "a") as file:
             file.write(emails)
         self.exclude_window.destroy()
         messagebox.showinfo("Success", "Emails will be ignored!")
 
     def clean_emails(self):
         if self.filepath:
-            data_cleaner = DataCleaner(self.filepath)
+            data_cleaner = DataCleaner(self.filepath, self.split_by_200.get())
             result = data_cleaner.clean_data()
             # Here you would add your logic to clean the emails based on ignore_emails list.
             if result:
